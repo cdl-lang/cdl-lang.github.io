83204fe18822e1009c2dd48be2911b42a6577ca3
Theo Vosse <theovosse@gmx.com>
2018-01-15 14:25:20 +0100

    testing to see if it runs from the designated url

---
+++ javascript/tmdb-demo.crt.js
@@ -0,0 +1,38 @@
+var fmtVersion = 2.3;
+
+// debuginfo=0 optimize=true errors= xflags= strictness=
+// start 0.005 s
+// nr PathTreeNodes 3
+// multiple paths 0
+// load js 0.004 s
+// remove qualifiers 0.002 s
+// create template tree 0.003 s
+// cycle 1 0 s
+// cycle 2 0.006 s
+// end 0 s
+var globalMessageNodeIndex = 0
+var pointerNodeIndex = 1
+var nrExports = 1
+var exportPaths = [null]
+var pathToExportId = {}
+
+var globalAreasUnderPointerNodeIndex = undefined
+var globalDefaultsNodeIndex = undefined
+_mq(0, 0, ["message"]) // %0G
+_ptr(0, 0, ["pointer"]) // %1G
+_v(["https://api.themoviedb.org/3/search/tv?api_key=e505bd0241a27561030c6acc5eebaeec&language=en-US&page=1&query=friends"], undefined) // %2G
+_f(0, 0, datatable, [_n(undefined,2)]) // %3G
+_f(1, 0, debugNodeToStr, [_n(undefined,3)]) // %4G
+_a(2, 0, {value:_nf(undefined,4)}, false) // %5G
+_a(3, 0, {text:_n(undefined,5)}, false) // %6G
+
+initAreaTemplate()
+_ac(undefined, undefined, 0, true) // area template create 1
+_mq(0, 0, ["message"]) // %0L1
+_par(0, 0, ["param"]) // %1L1
+_cnp(undefined)
+_ae(1, _n(undefined,6), undefined, undefined, undefined) // area template expr 1
+_mss(0, 3)
+_mss(1, 0)
+// compile time: 0.021 s
+// nr wont change: 0
